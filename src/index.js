6cdc7948959121d954c9ae5b6ffc40add4603763
Chris Penner <cpenner@vendasta.com>
2018-06-02 12:40:27 -0600

    Move app -> app/

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './app/App';
+import registerServiceWorker from './app/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
